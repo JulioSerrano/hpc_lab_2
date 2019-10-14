f85ab364f924c27ea5ff7cae7f9599c0ebca9299
Julio Serrano <julio.serrano@usach.cl>
2019-10-14 07:09:24 -0300

    change function for generate iomage

---
+++ image.py
@@ -0,0 +1,9 @@
+import numpy as np
+from matplotlib import pylab as plt
+
+A = np.fromfile("image.raw", dtype='int32')
+A = A.reshape([2001, 2001])
+
+print(A)
+plt.imshow(A)
+plt.show()
--- main.c
+++ main.c
@@ -14,6 +14,19 @@ Complex complexInit(float real, float imaginary) {
   return c;
 }
 
+float **createImage(int rows, int columns) {
+  float **image = (float **)malloc(sizeof(float *) * rows);
+  for (int i = 0; i < rows; i++) {
+    image[i] = (float *)malloc(sizeof(float) * columns);
+  }
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < columns; j++) {
+      image[i][j] = 0;
+    }
+  }
+  return image;
+}
+
 float complexAbsolute(Complex c) {
   return pow(c.real, 2) + pow(c.imaginary, 2);
 }
@@ -43,31 +56,72 @@ float mandelbrot(Complex c, int depth) {
   return log(n) + 1;
 }
 
+Complex **createComplexMatrix(float upperLimitReal, float lowerLimitReal,
+                              float upperLimitImaginary,
+                              float lowerLimitImaginary, double sampling) {
+
+  float rows = ((upperLimitReal - lowerLimitReal) / sampling) + 1;
+  float columns = ((upperLimitImaginary - lowerLimitImaginary) / sampling) + 1;
+
+  Complex **matrix = (Complex **)malloc(sizeof(Complex *) * rows);
+  for (int i = 0; i < rows; i++) {
+    matrix[i] = (Complex *)malloc(sizeof(Complex) * columns);
+  }
+  float i, j;
+  int x = 0, y = 0;
+  for (i = lowerLimitReal; i <= upperLimitReal; i += sampling) {
+    y = 0;
+    for (j = lowerLimitImaginary; j <= upperLimitImaginary; j += sampling) {
+      matrix[x][y].real = i;
+      matrix[x][y].imaginary = j;
+      y++;
+    }
+    x++;
+  }
+  return matrix;
+}
+
 int main() {
   int depth = 500;
-  float sampling = 0.001;
+  double sampling = 0.001;
   float upperLimitReal = 1;
   float lowerLimitReal = -1;
   float upperLimitImaginary = 1;
   float lowerLimitImaginary = -1;
 
   float i, j;
-  int n = (((upperLimitReal - lowerLimitReal) / sampling) + 1) *
-          (((upperLimitImaginary - lowerLimitImaginary) / sampling) + 1);
 
-  float *image = (float *)malloc(sizeof(float) * n);
-  n = 0;
-  for (i = lowerLimitReal; i <= upperLimitReal; i += sampling) {
-    for (j = lowerLimitImaginary; j <= upperLimitImaginary; j += sampling) {
-      Complex c = complexInit(i, j);
-      image[n] = mandelbrot(c, depth);
-      n += 1;
-      printf("%d\n", n);
+  float rows = ((upperLimitReal - lowerLimitReal) / sampling) + 1;
+  float columns = ((upperLimitImaginary - lowerLimitImaginary) / sampling) + 1;
+
+  // float *image = (float *)malloc(sizeof(float) * n);
+  // n = 0;
+  // for (i = lowerLimitReal; i <= upperLimitReal; i += sampling) {
+  //   for (j = lowerLimitImaginary; j <= upperLimitImaginary; j += sampling) {
+  //     Complex c = complexInit(i, j);
+  //     image[n] = mandelbrot(c, depth);
+  //     n += 1;
+  //     printf("%d\n", n);
+  //   }
+  // }
+
+  Complex **matrix =
+      createComplexMatrix(upperLimitReal, lowerLimitImaginary,
+                          upperLimitImaginary, lowerLimitImaginary, sampling);
+
+  float **image = createImage(rows, columns);
+
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < columns; j++) {
+      printf("%d %d\n", i, j);
+      image[i][j] = mandelbrot(matrix[i][j], depth);
     }
   }
 
   FILE *fp = fopen("image.raw", "w+");
-  for (int i = 0; i < n; i++) {
-    fwrite(&image[i], sizeof(float), 1, fp);
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < columns; j++) {
+      fwrite(&image[i][j], sizeof(float), 1, fp);
+    }
   }
 }
