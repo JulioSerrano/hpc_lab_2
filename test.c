332ac408d2ce674dec7609e8f7e394df7ca5c68e
Julio Serrano Pavez <julio.serrano@usach.cl>
2019-10-14 22:50:50 -0700

    WIP paralell

--- test.c
+++ test.c
@@ -6,19 +6,27 @@
 #endif
 
 typedef struct Complex {
-  float real;
-  float imaginary;
+  double real;
+  double imaginary;
 } Complex;
 
-Complex complexInit(float real, float imaginary) {
-  Complex c;
-  c.imaginary = imaginary;
-  c.real = real;
-  return c;
+double square(double n) { return n * n; }
+
+float **createImage(int rows, int columns) {
+  float **image = (float **)malloc(sizeof(float *) * rows);
+  for (int i = 0; i < rows; i++) {
+    image[i] = (float *)malloc(sizeof(float) * columns);
+  }
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < columns; j++) {
+      image[i][j] = 0;
+    }
+  }
+  return image;
 }
 
-float complexAbsolute(Complex c) {
-  return pow(c.real, 2) + pow(c.imaginary, 2);
+double complexAbsolute(Complex c) {
+  return sqrt((square(c.real) + square(c.imaginary)));
 }
 
 Complex complexSum(Complex c1, Complex c2) {
@@ -28,98 +36,90 @@ Complex complexSum(Complex c1, Complex c2) {
   return c;
 }
 
-Complex complexPow(Complex complex) {
+Complex complexsquare(Complex complex) {
   Complex c;
-  c.real = pow(complex.real, 2) + pow(complex.imaginary, 2) * -1;
+  c.real = square(complex.real) + square(complex.imaginary) * -1;
   c.imaginary = 2 * complex.real * complex.imaginary;
 
   return c;
 }
 
-float mandelbrot(Complex c, int depth) {
+double mandelbrot(Complex c, int depth) {
   Complex Zn = c;
   float n = 1;
   while (complexAbsolute(Zn) < 2 && n < depth) {
-    Zn = complexSum(complexPow(Zn), c);
+    Zn = complexSum(complexsquare(Zn), c);
     n += 1;
   }
   return log(n) + 1;
 }
 
-int main() {
-  int depth = 500;
-  float sampling = 0.5;
-  float upperLimitReal = 1;
-  float lowerLimitReal = -1;
-  float upperLimitImaginary = 1;
-  float lowerLimitImaginary = -1;
+double getDistance(double upperLimit, double lowerLimit, double sampling) {
+  return round((upperLimit - lowerLimit) / sampling) + 1;
+}
 
-  float i, j;
+double distance(double x, double y) {
+	return sqrt( square(x) + square(y) );
+}
 
-  float rows = ((upperLimitReal - lowerLimitReal) / sampling) + 1;
-  float columns = ((upperLimitImaginary - lowerLimitImaginary) / sampling) + 1;
-  float n = rows*columns;
-
-  float *image = (float *)malloc(sizeof(float) * n);
-  // n = 0;
-
-
-  float index, numth, mytid;
-  float N;
-  #pragma omp parallel private(mytid, numth, N, index) num_threads(2)
+void createMandelbrotImage(double lowerLimitImaginary, double lowerLimitReal,
+                           int rows, int columns, double sampling,
+                           int depth, float **image) {
+  
+  double Cx, Cy, Zn_r, Zn_i, Zn_aux;
+	int n, i, j;
+  # pragma omp parallel shared(image, sampling, depth, rows, columns) private(Cx, Cy, Zn_r, Zn_i, Zn_aux, n, i, j) num_threads(10)
   {
-
-  mytid = omp_get_thread_num();
-  numth = omp_get_num_threads();
-
-  printf("%d ",(int)mytid);
-  printf("%d\n",(int)numth);
-  N = ((upperLimitReal - lowerLimitReal)/numth);
-  index = lowerLimitReal+(mytid * N);
-  printf("N: %f\n", N);
-  printf("index: %f\n",index);
-
-
-  for (i = index; i <= N+index; i += sampling) {
-    for (j = lowerLimitImaginary; j <= upperLimitImaginary; j += sampling) {
-      Complex c = complexInit(i, j);
-      mandelbrot(c, depth);
-      printf("%f %f\n", i,j);
-      // n += 1;
-      // printf("%d\n", n);
+    # pragma omp for
+    for (i = 0; i < rows; i++) {
+      Cy = lowerLimitImaginary + sampling * i;
+      for (j = 0; j < columns; j++) {
+        Cx = lowerLimitReal + sampling * j;
+				n = 1;
+				Zn_r = Cx;
+				Zn_i = Cy;
+				while( distance(Zn_r, Zn_i) < 2 && n < depth) {
+					Zn_aux = square(Zn_r) + Cx - square(Zn_i);
+					Zn_i = 2*Zn_i*Zn_r + Cy;
+					Zn_r = Zn_aux;		
+					n++;
+				}
+				image[j][i] = log(n) + 1;
+      }
     }
   }
+}
 
+
+
+int main() {
+  int depth = 500;
+  double sampling = 0.00000000001;
+  double upperLimitReal = -0.748766707771757;
+  double lowerLimitReal = -0.748766713922161;
+  double upperLimitImaginary = 0.123640851045266;
+  double lowerLimitImaginary = 0.123640844894862;
+  // int depth = 500;
+  // double sampling = 0.001;
+  // double upperLimitReal = 1;
+  // double lowerLimitReal = -1;
+  // double upperLimitImaginary = 1;
+  // double lowerLimitImaginary = -1;
+
+  double i, j, rows, columns;
+  rows = getDistance(upperLimitReal, lowerLimitReal, sampling);
+  columns = getDistance(upperLimitImaginary, lowerLimitImaginary, sampling);
+
+  float **image = createImage(rows, columns);
+  
+  createMandelbrotImage(lowerLimitImaginary, lowerLimitReal, rows, columns,
+                        sampling, depth, image);
+
+  FILE *fp = fopen("image.raw", "w+");
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < columns; j++) {
+      fwrite(&image[i][j], sizeof(float), 1, fp);
+    }
   }
-
-
-
-
-  // int imagen[100000];
-
-  // int i, n, numth, mytid;
-
-  // #pragma omp parallel private(mytid, numth, n, i) num_threads(20)
-  // {
-  //   int j;
-  //   mytid = omp_get_thread_num();
-  //   numth = omp_get_num_threads();
-
-  //   printf("%d ",mytid);
-  //   printf("%d ",numth);
-  //   n = 100000/numth;
-  //   i = mytid * n;
-
-  //   #pragma omp for
-  //   for(j=i; j<n+i; j++)
-  //     printf("%d-",j);
-  //     imagen[j] = 2*imagen[j];
-  // }
-
-  // for(int i=0; i<100000; i++)
-  //   imagen[i] = 2*imagen[i];
-
-  // for(int i=0; i<100000; i++){
-  //   printf("%d ",imagen[i]);
-  // }
+  fclose(fp);
 }
